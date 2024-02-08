26f6265c56f8862d76e0b47389b544667ffb76f8
Michal Štrajt <93714867+mikeshtro@users.noreply.github.com>
2024-02-08 19:41:39 +0100

    Methods in template

--- src/morning-coffee/src/app/app.component.ts
+++ src/morning-coffee/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { CurrencyPipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 import { CoffeeType } from './coffee-type';
@@ -7,39 +8,54 @@ import { CoffeeOverviewComponent } from './pick-coffee/coffee-overview/coffee-ov
 @Component({
   selector: 'mcf-root',
   standalone: true,
-  imports: [CoffeeOverviewComponent],
+  imports: [CurrencyPipe, CoffeeOverviewComponent],
   template: `
-    @for (coffee of coffees; track coffee.id) {
-      <mcf-coffee-overview
-        [amount]="orderedCoffees.get(coffee.id)"
-        (amountChange)="orderCoffee($event, coffee.id)"
-      >
-        <span class="coffee">
-          @switch (coffee.id) {
-            @case ('espresso') {
-              Espresso
+    <div class="overview">
+      @for (coffee of coffeeTypes; track coffee) {
+        <mcf-coffee-overview
+          [amount]="orderedCoffees.get(coffee)"
+          (amountChange)="orderCoffee($event, coffee)"
+        >
+          <span class="coffee">
+            @switch (coffee) {
+              @case ('espresso') {
+                Espresso
+              }
+              @case ('doppio') {
+                Doppio
+              }
+              @case ('cappuccino') {
+                Cappuccino
+              }
             }
-            @case ('doppio') {
-              Doppio
-            }
-            @case ('cappuccino') {
-              Cappuccino
-            }
-          }
-        </span>
-      </mcf-coffee-overview>
-    }
+          </span>
+          <span>Price: {{ getPrice(coffee) }}</span>
+        </mcf-coffee-overview>
+      }
+    </div>
+    <div class="total">Total price: {{ getTotalPrice() }}</div>
   `,
   styles: `
+    .overview {
+      display: flex;
+      gap: 2rem;
+    }
+
     .coffee {
       color: #12ae12;
+    }
+
+    .total {
+      font-size: 2rem;
+      line-height: 1.75;
     }
   `,
 })
 export class AppComponent {
+  protected coffeeTypes: CoffeeType[] = ['espresso', 'doppio', 'cappuccino'];
   protected orderedCoffees = new Map<CoffeeType, number>();
 
-  protected readonly coffees: Coffee[] = [
+  private readonly coffeePrices: Coffee[] = [
     { id: 'espresso', price: 1.25 },
     { id: 'doppio', price: 1.75 },
     { id: 'cappuccino', price: 2.25 },
@@ -47,5 +63,27 @@ export class AppComponent {
 
   protected orderCoffee(amount: number, id: CoffeeType): void {
     this.orderedCoffees.set(id, amount);
+  }
+
+  protected getPrice(id: CoffeeType): number {
+    const price = this.coffeePrices.find(x => x.id === id)?.price;
+    const amount = this.orderedCoffees.get(id);
+
+    if (price == null || amount == null) {
+      return 0;
+    }
+
+    return price * amount;
+  }
+
+  protected getTotalPrice(): number {
+    let totalPrice = 0;
+
+    for (const price of this.coffeePrices) {
+      const amount = this.orderedCoffees.get(price.id) ?? 0;
+      totalPrice += amount * price.price;
+    }
+
+    return totalPrice;
   }
 }
